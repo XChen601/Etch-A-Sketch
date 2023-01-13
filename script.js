954b6c23537ec073dea09ffa0264e00626a80aa7
xchen <xchen601@gmail.com>
2023-01-13 14:37:47 -0500

    completed background, buttons styles, and finished with main etch a sketch

--- script.js
+++ script.js
@@ -1,30 +1,85 @@
-let rows = 15;
-let columns = 15;
+let rows = 64;
+let columns = 64;
+let rainbow = 'on';
 
 
+let root = document.documentElement;
 const container = document.querySelector('.container');
 
 
-for(let i = 0; i<rows; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
-    container.appendChild(rowDiv);
+createGrid();
+
+// new size button
+const newSizeBtn = document.querySelector('#newSizeBtn');
+newSizeBtn.addEventListener('click', (e)=> {
+    size = prompt("Enter a new desired size under 100:");
+    while (size > 100 || size == null){
+        size = prompt("Please enter a valid size:");
+    }
+    console.log(size)
+    rows = size;
+    columns = size;
+    clearGrid();
+
+})
+
+// clear button
+const clearBtn = document.querySelector('#clearBtn')
+clearBtn.addEventListener('click', (e) => {
+    clearGrid();
+})
+
+// rainbow button functions
+const rainbowBtn = document.querySelector('#rainbowBtn')
+rainbowBtn.addEventListener('click', (e) => {
+    rainbowBtn.classList.toggle('active');
+})
+
+function setBoxListeners(){
+    // add event listener to box and change box on hover
+    allBoxes = document.querySelectorAll('.box')
+
+    allBoxes.forEach(box => box.addEventListener('mouseover', (e) => {
+        if (rainbowBtn.classList.contains('active')) {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            box.style.backgroundColor = `#${randomColor}`
+        }
+        else {
+            box.style.backgroundColor = `black`
+        }
+        box.classList.add("hovered")
+    }))
 }
 
-rowElements = document.querySelectorAll('.row')
 
-// for each rows, add 16 divs
-
-rowElements.forEach(row => {
-    for(let i = 0; i<columns; i++){
-        const columnDiv = document.createElement('column')
-        columnDiv.classList.add('box');
-        row.appendChild(columnDiv);
+function createGrid(){
+    for(let i = 0; i<rows; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        container.appendChild(rowDiv);
     }
-    
-});
 
-allBoxes = document.querySelectorAll('.box')
+    rowElements = document.querySelectorAll('.row')
+
+    // for each rows, add 16 divs
+
+    rowElements.forEach(row => {
+        for(let i = 0; i<columns; i++){
+            const columnDiv = document.createElement('column')
+            columnDiv.classList.add('box');
+            row.appendChild(columnDiv);
+        }
+        
+    });
+    setBoxListeners();
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid();
+}
 
 
 
